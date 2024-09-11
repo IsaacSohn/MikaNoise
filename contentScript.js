bacba111af6dcae441b790304432bcce0b971335
IsaacSohn <isaacs0415@gmail.com>
2024-09-11 02:33:06 -0700

    yes.

--- contentScript.js
+++
@@ -1,8 +0,0 @@
-// Listen for messages from the service worker
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "playAudio") {
-        console.log("Playing audio in content script...");
-        const audio = new Audio(chrome.runtime.getURL('audio.mp3'));
-        audio.play();
-    }
-});
